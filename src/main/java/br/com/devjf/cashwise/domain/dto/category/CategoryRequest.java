95c4124deb69a61fc349f849b521d4170d3f3893
devjf <djfcoder@outlook.com>
2025-10-07 23:36:47 -0300

    fix: error message to category request

--- src/main/java/br/com/devjf/cashwise/domain/dto/category/CategoryRequest.java
+++ src/main/java/br/com/devjf/cashwise/domain/dto/category/CategoryRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record CategoryRequest(
         @NotBlank
-        @Size(max = 100)
+        @Size(max = 100, message = "O nome da categoria deve conter no máximo 100 caracteres")
         String name
         ) {
 
